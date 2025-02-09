c3140001b7966a8fffb01be4cfcf498f66ecf1eb
Nicholas Gower <nicholasgower01@gmail.com>
2025-02-09 09:47:21 -0800

    fix: Removed all control-level code related to cargo drops, ceding that functionality to PlanetsLib.

--- control.lua
+++ control.lua
@@ -18,39 +18,39 @@ script.on_event(defines.events.on_player_changed_surface, function(event)
     end
 end)
 
-script.on_event(defines.events.on_script_trigger_effect, function(event)
-    if event.effect_id == "cargo-pod-spawned" then
-        if event.cause_entity == nil then return end
-        if event.cause_entity.force.technologies["bioluminescent-navigation"].researched then return end
+-- script.on_event(defines.events.on_script_trigger_effect, function(event)
+--     if event.effect_id == "cargo-pod-spawned" then
+--         if event.cause_entity == nil then return end
+--         if event.cause_entity.force.technologies["planetslib-tenebris-cargo-drops"].researched then return end
 
-        if event.cause_entity.surface.name == TENEBRIS and event.cause_entity.has_items_inside() then
-            local source_inventory = event.cause_entity.get_inventory(defines.inventory.cargo_landing_pad_main)
+--         if event.cause_entity.surface.name == TENEBRIS and event.cause_entity.has_items_inside() then
+--             local source_inventory = event.cause_entity.get_inventory(defines.inventory.cargo_landing_pad_main)
 
-            local distance = math.random(2000, 2500)
-            local angle = math.random() * math.pi * 2
+--             local distance = math.random(2000, 2500)
+--             local angle = math.random() * math.pi * 2
 
-            local x = math.sin(angle) * distance
-            local y = math.cos(angle) * distance
+--             local x = math.sin(angle) * distance
+--             local y = math.cos(angle) * distance
 
-            local pod = event.cause_entity.surface.create_entity {
-                name = "cargo-pod-container", force = event.cause_entity.force, position = { x, y }
-            }
-            local target_inventory = pod.get_inventory(defines.inventory.cargo_landing_pad_main)
+--             local pod = event.cause_entity.surface.create_entity {
+--                 name = "cargo-pod-container", force = event.cause_entity.force, position = { x, y }
+--             }
+--             local target_inventory = pod.get_inventory(defines.inventory.cargo_landing_pad_main)
 
-            event.cause_entity.force.chart(event.cause_entity.surface, { { x - 10, y - 10 }, { x + 10, y + 10 } })
-            game.print("Cargo pod navigation failure, crash landed at " .. pod.gps_tag)
+--             event.cause_entity.force.chart(event.cause_entity.surface, { { x - 10, y - 10 }, { x + 10, y + 10 } })
+--             game.print("Cargo pod navigation failure, crash landed at " .. pod.gps_tag)
 
-            if source_inventory and target_inventory then
-                for i = 1, #source_inventory do
-                    local stack = source_inventory[i]
+--             if source_inventory and target_inventory then
+--                 for i = 1, #source_inventory do
+--                     local stack = source_inventory[i]
 
-                    if stack.valid_for_read then
-                        target_inventory.insert(stack)
-                    end
-                end
+--                     if stack.valid_for_read then
+--                         target_inventory.insert(stack)
+--                     end
+--                 end
 
-                event.cause_entity.destroy()
-            end
-        end
-    end
-end)
+--                 event.cause_entity.destroy()
+--             end
+--         end
+--     end
+-- end)
