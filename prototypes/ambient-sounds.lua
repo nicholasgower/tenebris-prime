3640fcbb57562eba0ab3e226bd7a40bbafdafcb9
Nicholas Gower <nicholasgower01@gmail.com>
2025-03-01 20:17:01 -0800

    sound: Removed all ambient music to fix game freeze.

--- prototypes/ambient-sounds.lua
+++ prototypes/ambient-sounds.lua
@@ -1,179 +1,179 @@
-data:extend(
-{
---- weight 10 is normal. The higher the number, the higher the probabilities to sound.
+-- data:extend(
+-- {
+-- --- weight 10 is normal. The higher the number, the higher the probabilities to sound.
 
--------------------------------------------------------------------------------------tenebris
+-- -------------------------------------------------------------------------------------tenebris
 
---   {
---     type = "ambient-sound",
---     name = "tenebris-1",
---     track_type = "main-track",
---     planet = "tenebris",
---     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-1.ogg",
---     weight = 10,
---   },
---   {
---     type = "ambient-sound",
---     name = "tenebris-2",
---     track_type = "main-track",
---     planet = "tenebris",
---     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-2.ogg",
---     weight = 10,
---   },
 -- --   {
 -- --     type = "ambient-sound",
--- --     name = "tenebris-3",
+-- --     name = "tenebris-1",
 -- --     track_type = "main-track",
 -- --     planet = "tenebris",
--- --     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-3.ogg",
+-- --     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-1.ogg",
 -- --     weight = 10,
 -- --   },
---   {
---     type = "ambient-sound",
---     name = "tenebris-4",
---     track_type = "main-track",
---     planet = "tenebris",
---     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-4.ogg",
---     weight = 10,
---   },
---   {
---     type = "ambient-sound",
---     name = "tenebris-5",
---     track_type = "main-track",
---     planet = "tenebris",
---     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-5.ogg",
---     weight = 10,
---   },
-  {
-    type = "ambient-sound",
-    name = "tenebris-6",
-    track_type = "main-track",
-    planet = "tenebris",
-    sound = "__tenebris-prime__/sound/ambient/tenebris/Darkening Developments.ogg",
-    weight = 10,
-  },
-  {
-    type = "ambient-sound",
-    name = "tenebris-7",
-    track_type = "main-track",
-    planet = "tenebris",
-    sound = "__tenebris-prime__/sound/ambient/tenebris/Darkness Speaks.ogg",
-    weight = 10,
-  },
-  {
-    type = "ambient-sound",
-    name = "tenebris-8",
-    track_type = "main-track",
-    planet = "tenebris",
-    sound = "__tenebris-prime__/sound/ambient/tenebris/Dark Pad.ogg",
-    weight = 10,
-  },
-  {
-    type = "ambient-sound",
-    name = "tenebris-9",
-    track_type = "main-track",
-    planet = "tenebris",
-    sound = "__tenebris-prime__/sound/ambient/tenebris/Gathering Darkness.ogg",
-    weight = 10,
-  },
-  {
-    type = "ambient-sound",
-    name = "tenebris-10",
-    track_type = "main-track",
-    planet = "tenebris",
-    sound = "__tenebris-prime__/sound/ambient/tenebris/Shadowlands 7 - Codex.ogg",
-    weight = 10,
-  },
-  {
-    type = "ambient-sound",
-    name = "tenebris-11",
-    track_type = "main-track",
-    planet = "tenebris",
-    sound = "__tenebris-prime__/sound/ambient/tenebris/That Zen Moment.ogg",
-    weight = 10,
-  },
---   {
---     type = "ambient-sound",
---     name = "gleba-1",
---     track_type = "main-track",
---     planet = "gleba",
---     sound = "__space-age__/sound/ambient/gleba/gleba-1.ogg",
---     weight = 10
---   },
---   {
---     type = "ambient-sound",
---     name = "gleba-2",
---     track_type = "main-track",
---     planet = "gleba",
---     sound = "__space-age__/sound/ambient/gleba/gleba-2.ogg",
---     weight = 10
---   },
---   {
---     type = "ambient-sound",
---     name = "gleba-3",
---     track_type = "main-track",
---     planet = "gleba",
---     sound = "__space-age__/sound/ambient/gleba/gleba-3.ogg",
---     weight = 10
---   },
---   {
---     type = "ambient-sound",
---     name = "gleba-4",
---     track_type = "main-track",
---     planet = "gleba",
---     sound = "__space-age__/sound/ambient/gleba/gleba-4.ogg",
---     weight = 8
---   },
---   {
---     type = "ambient-sound",
---     name = "gleba-5",
---     track_type = "main-track",
---     planet = "gleba",
---     sound = "__space-age__/sound/ambient/gleba/gleba-5.ogg",
---     weight = 10
---   },
---   {
---     type = "ambient-sound",
---     name = "gleba-6",
---     track_type = "main-track",
---     planet = "gleba",
---     sound = "__space-age__/sound/ambient/gleba/gleba-6.ogg",
---     weight = 8
---   },
---   {
---     type = "ambient-sound",
---     name = "gleba-8",
---     track_type = "main-track",
---     planet = "gleba",
---     sound = "__space-age__/sound/ambient/gleba/gleba-8.ogg",
---     weight = 10
---   },
---   {
---     type = "ambient-sound",
---     name = "gleba-9",
---     track_type = "main-track",
---     planet = "gleba",
---     sound = "__space-age__/sound/ambient/gleba/gleba-9.ogg",
---     weight = 8
---   },
---    {
---     type = "ambient-sound",
---     name = "gleba-10",
---     track_type = "main-track",
---     planet = "gleba",
---     sound =
---     {
---       variations =
---       {
---         "__space-age__/sound/ambient/gleba/gleba-10-a.ogg",
---         "__space-age__/sound/ambient/gleba/gleba-10-b.ogg",
---         "__space-age__/sound/ambient/gleba/gleba-10-c.ogg",
---       },
---     },
---     weight = 12,
---   },
---   require("__space-age__/sound/ambient/gleba/gleba-11/gleba-11"),
---   require("__space-age__/sound/ambient/gleba/interlude-1/interlude-1"),
---   require("__space-age__/sound/ambient/gleba/interlude-4/interlude-4"),
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "tenebris-2",
+-- --     track_type = "main-track",
+-- --     planet = "tenebris",
+-- --     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-2.ogg",
+-- --     weight = 10,
+-- --   },
+-- -- --   {
+-- -- --     type = "ambient-sound",
+-- -- --     name = "tenebris-3",
+-- -- --     track_type = "main-track",
+-- -- --     planet = "tenebris",
+-- -- --     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-3.ogg",
+-- -- --     weight = 10,
+-- -- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "tenebris-4",
+-- --     track_type = "main-track",
+-- --     planet = "tenebris",
+-- --     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-4.ogg",
+-- --     weight = 10,
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "tenebris-5",
+-- --     track_type = "main-track",
+-- --     planet = "tenebris",
+-- --     sound = "__tenebris-prime__/sound/ambient/terrapalus/terrapalus-5.ogg",
+-- --     weight = 10,
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "tenebris-6",
+-- --     track_type = "main-track",
+-- --     planet = "tenebris",
+-- --     sound = "__tenebris-prime__/sound/ambient/tenebris/Darkening-Developments.ogg",
+-- --     weight = 10,
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "tenebris-7",
+-- --     track_type = "main-track",
+-- --     planet = "tenebris",
+-- --     sound = "__tenebris-prime__/sound/ambient/tenebris/Darkness-Speaks.ogg",
+-- --     weight = 10,
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "tenebris-8",
+-- --     track_type = "main-track",
+-- --     planet = "tenebris",
+-- --     sound = "__tenebris-prime__/sound/ambient/tenebris/Dark-Pad.ogg",
+-- --     weight = 10,
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "tenebris-9",
+-- --     track_type = "main-track",
+-- --     planet = "tenebris",
+-- --     sound = "__tenebris-prime__/sound/ambient/tenebris/Gathering-Darkness.ogg",
+-- --     weight = 10,
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "tenebris-10",
+-- --     track_type = "main-track",
+-- --     planet = "tenebris",
+-- --     sound = "__tenebris-prime__/sound/ambient/tenebris/Shadowlands7-Codex.ogg",
+-- --     weight = 10,
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "tenebris-11",
+-- --     track_type = "main-track",
+-- --     planet = "tenebris",
+-- --     sound = "__tenebris-prime__/sound/ambient/tenebris/That Zen Moment.ogg",
+-- --     weight = 10,
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "gleba-1",
+-- --     track_type = "main-track",
+-- --     planet = "gleba",
+-- --     sound = "__space-age__/sound/ambient/gleba/gleba-1.ogg",
+-- --     weight = 10
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "gleba-2",
+-- --     track_type = "main-track",
+-- --     planet = "gleba",
+-- --     sound = "__space-age__/sound/ambient/gleba/gleba-2.ogg",
+-- --     weight = 10
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "gleba-3",
+-- --     track_type = "main-track",
+-- --     planet = "gleba",
+-- --     sound = "__space-age__/sound/ambient/gleba/gleba-3.ogg",
+-- --     weight = 10
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "gleba-4",
+-- --     track_type = "main-track",
+-- --     planet = "gleba",
+-- --     sound = "__space-age__/sound/ambient/gleba/gleba-4.ogg",
+-- --     weight = 8
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "gleba-5",
+-- --     track_type = "main-track",
+-- --     planet = "gleba",
+-- --     sound = "__space-age__/sound/ambient/gleba/gleba-5.ogg",
+-- --     weight = 10
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "gleba-6",
+-- --     track_type = "main-track",
+-- --     planet = "gleba",
+-- --     sound = "__space-age__/sound/ambient/gleba/gleba-6.ogg",
+-- --     weight = 8
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "gleba-8",
+-- --     track_type = "main-track",
+-- --     planet = "gleba",
+-- --     sound = "__space-age__/sound/ambient/gleba/gleba-8.ogg",
+-- --     weight = 10
+-- --   },
+-- --   {
+-- --     type = "ambient-sound",
+-- --     name = "gleba-9",
+-- --     track_type = "main-track",
+-- --     planet = "gleba",
+-- --     sound = "__space-age__/sound/ambient/gleba/gleba-9.ogg",
+-- --     weight = 8
+-- --   },
+-- --    {
+-- --     type = "ambient-sound",
+-- --     name = "gleba-10",
+-- --     track_type = "main-track",
+-- --     planet = "gleba",
+-- --     sound =
+-- --     {
+-- --       variations =
+-- --       {
+-- --         "__space-age__/sound/ambient/gleba/gleba-10-a.ogg",
+-- --         "__space-age__/sound/ambient/gleba/gleba-10-b.ogg",
+-- --         "__space-age__/sound/ambient/gleba/gleba-10-c.ogg",
+-- --       },
+-- --     },
+-- --     weight = 12,
+-- --   },
+-- --   require("__space-age__/sound/ambient/gleba/gleba-11/gleba-11"),
+-- --   require("__space-age__/sound/ambient/gleba/interlude-1/interlude-1"),
+-- --   require("__space-age__/sound/ambient/gleba/interlude-4/interlude-4"),
 
-})
+-- })
